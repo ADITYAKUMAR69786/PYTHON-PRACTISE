c8a6f6f319d0d538ea2da3076caff4d8d91c9130
ADITYA KUMAR VERMA <77196597+ADITYAKUMAR69786@users.noreply.github.com>
2023-04-25 21:29:30 +0530

    Add files via upload

---
+++ website.py
@@ -0,0 +1,19 @@
+import webbrowser
+import media
+import os
+Movie():
+def __init__(self,m_title,m_storyline,m_posterimage,m_trailer):
+    self.m_title = m_title
+    self.m_storyline = m_storyline
+    self.m_posterimage = m_posterimage
+    self.m_trailer = m_trailer
+
+def show_trailer(self):
+    webbrowser.open(self.m_trailer)
+
+def __str__(self):
+    return f'Movie Name is {self.m_title})
+
+
+Thor = Movie('Ek Tha Tiger','The fugitive spy', "E:\DOWNLOADS 2")
+print(ek_tha_tiger)
